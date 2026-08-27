790f0adb92c15723b6b7990c34927da3df8ccaf9
Harsh thapa <ayushthapa112@gmail.com>
2026-08-27 11:25:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (35.91%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(int n) {
+        int og = n;
+        int revN = 0;
+
+        while( n > 0){
+            int lastdigit = n % 10;
+            n = n / 10;
+             if (revN > INT_MAX / 10 || 
+                (revN == INT_MAX / 10 && lastdigit > 7)) {
+                return 0;
+            }
+
+            if (revN < INT_MIN / 10 || 
+                (revN == INT_MIN / 10 && lastdigit < -8)) {
+                return 0;
+            }
+
+
+            revN = (revN * 10) + lastdigit;
+        }
+
+        if(revN == og){
+            return true;
+        }
+        return false;
+    }
+};
